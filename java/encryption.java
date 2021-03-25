3a344e188398c62906d513ce350a7a2c612b77dd
KokatsuKitsune <kendricksmith@creighton.edu>
2021-03-25 14:55:27 -0500

    Update encryption.java

--- java/encryption.java
+++ java/encryption.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-function String Encrypt(String string){
+function String reverseEncrypt(String string){
   ArrayList<String> arr = Arrays.asList(string.split(""));
   String out = ""
   for(int i = arr.size()-1; i >= 0 ; i--) {
