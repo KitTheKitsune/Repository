76f00fe679342f33fac5d678e49fa3513e8aecba
KokatsuKitsune <kendricksmith@creighton.edu>
2021-05-18 12:15:25 -0500

    Update Hamiltonian.java

--- java/Hamiltonian.java
+++ java/Hamiltonian.java
@@ -1,39 +1,101 @@
 import java.util.ArrayList;
 
+//DEFINITIONS
+//A WALK IS A SERIES OF ALTERNATING VERTICES AND EDGES THAT BEGINS AND ENDS WITH A VERTEX IN WHICH ALL ELEMENTS ARE INCIDENT
+      //AN OPEN WALK IF THE STARTING AND ENDING VERTICES ARE DIFFERENT
+      //A CLOSED WALK IF THE STARTING AND ENDING VERTICES ARE THE SAME
+//A TRAIL IS AN OPEN WALK WITHOUT REPEATED EDGES
+//A PATH IS A TRAIL WITHOUT REPEATED VERTICES
+//A HAMILTONIAN PATH IS A PATH THAT VISITS ALL VERTICES
+
+//A CIRCUIT IS A CLOSED WALK WITHOUT REPEATED EDGES
+//CYCLE IS A CIRCUIT WITHOUT REPEATED VERTICES
+//A HAMILTONIAN CYCLE IS A CYCLE THAT VISITS ALL VERTICES
+
+
+
 class Hamiltonian {
-  private int arraySize;
+  private int graphSize;
   private IndexPair start;
   private ArrayList<IndexPair> path;
   private ArrayList<IndexPair> cycle;
   
-  public Hamiltonian(array2d){
-    this.arraySize = array2d.size;
-    if (array2d[0].size > this.arraySize){
-      this.arraySize = array2d[0].size;
-    }
-    this.start = new IndexPair(0,0);
-    this.path = new ArrayList<IndexPair>(arraySize * arraySize);
+  public Hamiltonian(int[] array2d){
   }
   
-  public ArrayList<IndexPair> hamiltonianPath(start){
-    ArrayList<IndexPair> path = generatePath(start);
-    if (isValidPath(path)){
-      return path;
-    }else{
-      return null;
-    }
+  /**
+  *finds a valid hamiltonian path if one exists for the start point given
+  *@return returns a valid path or null if a valid path does not exist
+  */
+  private ArrayList<IndexPair> genHamiltonianPath(){
+    return null;
   }
   
-  public void hamiltonianCycle(){
+  /**
+  *finds a valid hamiltonian cycle if one exists
+  *@return a valid cycle or null if a valid cycle does not exist
+  */
+  private ArrayList<IndexPair> genHamiltonianCycle(){
+    return null;
   }
+  
+  /**
+  *
+  *@return boolean if path is valid hamiltonian
+  */
+  private boolean isValidPath(ArrayList<IndexPair> path){
+    return false;
+  }
+  
+  /**
+  */
+  private boolean isValidCycle(ArrayList<IndexPair> cycle){
+    return false;
+  }
+  
+  /**
+  */
+  private boolean isValidHamiltonian(ArrayList<IndexPair> ham){
+    return false;
+  }
+  
+  /**
+  */
+  private ArrayList<IndexPair> generateWalk(){
+    return null;
+  }
+  
+  
+  //GETTERS
+  public int getGraphSize(){
+    return this.graphSize;
+  }
+  public IndexPair getStart(){
+    return this.start;
+  }
+  public ArrayList<IndexPair> getPath(){
+    return this.path;
+  }
+  public ArrayList<IndexPair> getCycle(){
+    return this.cycle;
+  }
+  
   
   class IndexPair {
-    private x;
-    private y;
+    private int x;
+    private int y;
     
-    public IndexPair(x,y){
+    protected IndexPair(int x, int y){
       this.x = x;
       this.y = y;
+    }
+    
+    //GETTERS
+    protected int getX(){
+      return this.x;
+    }
+    protected int getY(){
+      return this.y;
     }
   }
 }
